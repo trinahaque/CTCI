876c6eba0c3d32e6dc952d2489a1335643f84def
Trina Haque <trina_haque28@outlook.com>
2018-04-23 17:10:54 -0700

    checkPermutation if given two strings

--- checkPermutation.js
+++ checkPermutation.js
@@ -1,5 +1,9 @@
 // Check Permutation
 // Given two strings, write a method to decide if one is a permutation of the other
+// Two methods: sort both strings and compare it. Time Complexity O(nlgn). 
+// Use Hash Table: Time Complexity O(n) --> n + n + n
+// Use dictionary with key value pair
+
 
 function checkPermutation(str1, str2){
     if (str1.length !== str2.length){
@@ -12,4 +16,43 @@ function checkPermutation(str1, str2){
         return false;
     }
 
+    var dict = { }
+
+    // go through str1
+    for (var i = 0; i < str1.length; i++){
+        if (!(str1[i] in dict)) {
+            dict[str1[i]] = 1;
+        }
+        else{
+            dict[str1[i]] += 1;
+        }
+    }
+
+    // go through str2
+    for (var j = 0; j < str2.length; j++){
+        if (!(str2[j] in dict)){
+            // it basically means if any chr not in dict
+            return false;
+        }
+        else {
+            dict[str2[j]] -= 1;
+        }
+    }
+
+    // console.log("dict after first str is", dict);
+
+    for (key in dict){
+        if (dict[key] !== 0){
+            return false;
+        }
+    }
+
+    return true;
 }
+
+console.log(checkPermutation("ttttt", "ttatt"));
+
+
+
+
+// https://algorithms.tutorialhorizon.com/find-whether-two-strings-are-permutation-of-each-other/
