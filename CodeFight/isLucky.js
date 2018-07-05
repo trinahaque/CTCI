be46cd036de70b0091f29a76636006ef1558e941
Trina Haque <v-trhaqu@microsoft.com>
2018-07-05 15:41:11 -0700

    refactored code

--- CodeFight/isLucky.js
+++ CodeFight/isLucky.js
@@ -1,22 +1,15 @@
 function isLucky(n) {
-    // console.log("num", n[0]);
     var newArray = (n + '').split('');
-    // console.log(numArray);
-    var numArray = [];
-    for (var i = 0; i <newArray.length; i++){
-        numArray.push(parseInt(newArray[i]));
-    }
-    // console.log(numArray);
     var sum1 = 0;
     var sum2 = 0;
-    for (var i = 0; i < numArray.length / 2; i++){
-        // console.log("num is ", parseInt(numArray[i]));
-        sum1 += numArray[i];
-        sum2 += numArray[numArray.length - 1 - i];
+    
+    for (var i = 0; i < newArray.length / 2; i++){
+        sum1 += parseInt(newArray[i]);
+        sum2 += parseInt(newArray[newArray.length - 1 - i]);
         // console.log("sum", sum1, sum2);
     }
+    
     if (sum1 == sum2){
-        // console.log("sum", sum1, sum2);
         return true;
     }
     else{
@@ -24,4 +17,4 @@ function isLucky(n) {
     }
 }
 
-console.log(isLucky(1230));
+console.log(isLucky(1220));
