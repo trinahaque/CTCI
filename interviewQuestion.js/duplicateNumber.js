d2cff10e9290199cfcc279766e74ae88fbf4ee80
Trina Haque <v-trhaqu@microsoft.com>
2018-07-07 13:32:21 -0700

    solved duplicateNumber in an array

---
+++ interviewQuestion.js/duplicateNumber.js
@@ -0,0 +1,25 @@
+// Given an unsorted list of numbers, find and return any duplicates in the list.
+// [1,2,5,2,4,6,3,4,2,3] --> [2,3,4]
+// On space --> dict
+// On time --> one for loop and one for checking keys in dict
+
+function duplicateNumber(arr){
+    var dict = {};
+    var resultArray = [];
+    for (var i = 0; i < arr.length; i++){
+        if (arr[i] in dict){
+            dict[arr[i]]++;
+        }
+        else{
+            dict[arr[i]] = 1;
+        }
+    }
+    for (key in dict){
+        if (dict[key] > 1){
+            resultArray.push(parseInt(key));
+        }
+    }
+    return resultArray;
+}
+
+console.log(duplicateNumber([1,2,5,2,4,6,3,4,2,3]));
