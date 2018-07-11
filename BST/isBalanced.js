1cc56666f7b1e24cde99a39de0f013c52a25a553
Trina Haque <v-trhaqu@microsoft.com>
2018-07-10 22:00:40 -0700

    finished isBalanced

---
+++ BST/isBalanced.js
@@ -0,0 +1,67 @@
+BST.prototype.isBalanced = function(){
+    if (!this.root){
+        return false;
+    }
+    return this.root.isBalanced(Infinity, Infinity);
+}
+BSTNode.prototype.isBalanced = function(min, max){
+    if (this == null){
+        return true;
+    }
+    else if (this.val < min || this.val > max){
+        return false;
+    }
+    else{
+        return this.left.isBalanced(min, this.val) && this.right.isBalanced(this.val, max);
+    }
+
+}
+
+
+function BST(){
+    this.root = null;
+}
+  
+function BSTNode(val){
+    this.val = val;
+    this.left = null;
+    this.right = null;
+}
+  
+BST.prototype.add = function(val){
+    if (!this.root){
+      this.root = new BSTNode(val);
+      return this;
+    }
+    this.root.add(val);
+    return this;
+}
+  
+BSTNode.prototype.add = function(val){
+    var node = new BSTNode(val);
+    if (val == this.val){
+      return this;
+    }
+    if (val < this.val){
+      if (!this.left){
+        this.left = node;
+        return this;
+      }
+      else{
+        this.left.add(val);
+      }
+    }
+    else{
+      if (!this.right){
+        this.right = node;
+        return this;
+      }
+      else{
+        this.right.add(val);
+      }
+    }
+}
+
+bst = new BST();
+bst.add(30).add(20).add(40).add(10).add(25).add(35).add(50);
+console.log(bst.isBalanced());
