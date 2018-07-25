1671d08089e56d8a68cde9690fd39b3aba5a43f9
Trina Haque <v-trhaqu@microsoft.com>
2018-07-25 14:38:26 -0700

    fib with prototype

--- DynamicProgramming/fiboCache.js
+++ DynamicProgramming/fiboCache.js
@@ -11,7 +11,7 @@ function fibonacci(n){
     return memo[n];
 }
 
-// console.log(fibonacci(8));
+console.log(fibonacci(8));
 
 var fibonacci = (function(n){
     var memo = [];
@@ -31,17 +31,39 @@ var fibonacci = (function(n){
 
 // console.log(fibonacci(8));
 
-function fibonacciNormal(n){
-    if (n == 0 || n == 1){
-        return n;
-    }
-    return fibonacciNormal(n-1) + fibonacciNormal(n - 2);
+// function fibonacciNormal(n){
+//     if (n == 0 || n == 1){
+//         return n;
+//     }
+//     return fibonacciNormal(n-1) + fibonacciNormal(n - 2);
+// }
+
+// console.log(fibonacciNormal(0));
+// console.log(fibonacciNormal(1));
+// console.log(fibonacciNormal(2));
+// console.log(fibonacciNormal(3));
+// console.log(fibonacciNormal(4));
+// console.log(fibonacciNormal(5));
+// console.log(fibonacciNormal(6));
+
+
+function fib(n){
+    this.memo = [];
+    return this.fibonacci(n);
 }
 
-console.log(fibonacciNormal(0));
-console.log(fibonacciNormal(1));
-console.log(fibonacciNormal(2));
-console.log(fibonacciNormal(3));
-console.log(fibonacciNormal(4));
-console.log(fibonacciNormal(5));
-console.log(fibonacciNormal(6));
+fib.prototype.fibonacci = function(n){
+    // console.log("memo ", memo);
+    if (this.memo[n]){
+        return this.memo[n];
+    }
+    if (n == 0 || n == 1){
+        this.memo[n] = n;
+    }
+    else{
+        this.memo[n] = this.fibonacci(n-1) + this.fibonacci(n-2);
+    }
+    return this.memo[n];
+}
+
+console.log(fib(6));
