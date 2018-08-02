40e3733e5f808b37a1a47cf18172b047890c0370
Trina Haque <v-trhaqu@microsoft.com>
2018-08-02 01:11:34 -0700

    remove a node

--- BST/removeNode.js
+++ BST/removeNode.js
@@ -37,3 +37,56 @@ BSTNode.prototype.maxNode = function(){
     }
     return this.right.maxNode();
 }
+
+// removing a node
+BST.prototype.delete = function(val){
+    if (!this.root){
+        return null;
+    }
+    return this.root.delete(val);
+}
+
+BSTNode.prototype.delete = function(val){
+    var dict = this.findNode(val);
+    // dict is an object. It should have found(true/false), parent, side, node
+    if (dict.found){
+        // deleting a leaf node, set the parent node side to null
+        if (!dict.node.left && !dict.node.right){
+            dict.parent.side = null;
+        }
+        // if deleted node only has once child, connect the parent node side to child
+        else if (!dict.node.left && dict.node.right){
+            dict.parent.side = dict.node.right;
+        }
+        else if (!dict.node.right && dict.node.left){
+            dict.parent.side = dict.node.left;
+        }
+        // if node has both children, connect the parent to the min of the right node
+        // find the left most of the min node. Connect the right child of the found 
+        // node to the left side of the leftmost node
+        else if (dict.node.left && dict.node.right){
+            // connect the parent to the right child of the node
+            dict.parent.side = dict.node.right;
+            var leftMostNode = dict.node.right.leftMost();
+            leftMostNode.left = dict.node.left;
+        }
+
+    }
+    return null;
+}
+
+
+// find the left most Node
+BST.prototype.leftMost = function(){
+    if (!this.root){
+        return null;
+    }
+    return this.root.leftMost();
+}
+
+BSTNode.prototype.leftMost = function(){
+    if (!this.left){
+        return this;
+    }
+    return this.left.leftMost();
+}
