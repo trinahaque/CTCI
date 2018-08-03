261be4b02ff3ec034bae4698e8979dceaffb125b
Trina Haque <v-trhaqu@microsoft.com>
2018-08-03 01:12:41 -0700

    regular fibonacci

--- DynamicProgramming/fiboCache.js
+++ DynamicProgramming/fiboCache.js
@@ -1,3 +1,21 @@
+// fibonacci without cache
+function fib(n){
+    if (n < 2){
+        return n;
+    }
+    return fib(n-1) + fib(n-2);
+}
+
+console.log(fib(0));
+console.log(fib(1));
+console.log(fib(2));
+console.log(fib(3));
+console.log(fib(4));
+console.log(fib(5));
+console.log(fib(6));
+
+
+// fibonacci with cache
 function fib2(n){
     var arr = [];
     function fib(n){
