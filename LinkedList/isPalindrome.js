281f89db3c8085d862d51f91f9de93663b328f18
Trina Haque <v-trhaqu@microsoft.com>
2018-08-03 21:58:48 -0700

    linked list isPalindrome

---
+++ LinkedList/isPalindrome.js
@@ -0,0 +1,121 @@
+function SLL(){
+    this.head = null;
+    this.length = 0;
+}
+
+function SLNode(val){
+    this.val = val;
+    this.next = null;
+}
+
+SLL.prototype.add = function(val){
+    var new_node = new SLNode(val);
+    if (!this.head){
+        this.head = new_node;
+    //   this.tail = new_node;
+        this.length += 1;
+        return this;
+    }
+    var current = this.head;
+    while (current.next){
+        current = current.next;
+    }
+    current.next = new_node;
+    // this.tail = new_node;
+    this.length += 1;
+    return this;
+}
+
+
+// given a linked list, determine if it's palindrome
+SLL.prototype.isPalindrome = function(){
+    if (!this.head){
+        return null;
+    }
+    var count = 0;
+    var current = this.head;
+    // finding the length of the sll
+    while (current){
+        count++;
+        current = current.next;
+    }
+    // need the higher value from the mid
+    var mid = Math.ceil(count / 2);
+    // console.log("mid ", mid);
+    var stack = [];
+    var queue = new Queue();
+    current = this.head;
+    var odd = false;
+    if (count % 2 != 0){
+        odd = true;
+    }
+
+    while (count > mid){
+        stack.push(current.val);
+        current = current.next;
+        count--;
+    }
+    // console.log(stack);
+    // console.log("current is ", current.val);
+    if (odd){
+        current = current.next;
+    }
+    // console.log("current is ", current.val);
+    while (current){
+        queue.enqueue(current.val);
+        current = current.next;
+    }
+    // console.log("q ", queue);
+    while (stack.length != 0){
+        if (stack.pop() != queue.dequeue().val){
+            return false;
+        }
+    }
+    return true;
+    
+}
+
+function Queue(){
+    this.head = null;
+    this.length = 0;
+  }
+  
+  function Node(val){
+    this.val = val;
+    this.next = null;
+  }
+  
+Queue.prototype.enqueue = function(val){
+    var node = new Node(val);
+    if (!this.head){
+      this.head = node;
+      this.tail = node;
+      this.length += 1;
+      return this;
+    }
+    this.tail.next = node;
+    this.tail = this.tail.next;
+    this.length += 1;
+    this.tail.next = null;
+    return this;
+}
+  
+Queue.prototype.dequeue = function(){
+    if (!this.head){
+      return null;
+    }
+    var tmp = this.head;
+    this.head = this.head.next;
+    this.length -= 1;
+    if (this.length == 0){
+      this.tail = null;
+    }
+    return tmp;
+}
+
+
+
+
+var sll = new SLL();
+sll.add("t").add("a").add("c").add("o").add("c").add("a").add("m");
+console.log(sll.isPalindrome());
